d6cc623d67e4761c46a5eaed77dff2e268ac2700
Jeff Pipas <jpipas@gmail.com>
2012-09-06 00:41:25 -0400

    started work on dashboard - updated to extjs 4.1.x

--- CRM/app/view/override/ContentPanel.js
+++
@@ -1,7 +0,0 @@
-Ext.define('JavisERP.view.override.ContentPanel', {
-    requires: 'JavisERP.view.ContentPanel'
-}, function() {
-    Ext.override(JavisERP.view.ContentPanel, {
-        
-    });
-});
--- CRM/app/view/override/PortalColumn.js
+++
@@ -1,7 +0,0 @@
-Ext.define('JavisERP.view.override.PortalColumn', {
-    requires: ['JavisERP.view.PortalColumn','JavisERP.view.Portlet']
-}, function() {
-    Ext.override(JavisERP.view.PortalColumn, {
-        
-    });
-});
